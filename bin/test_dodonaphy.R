6429709e22f6173b9542d85b108b1053e66854aa
koadman <>
2021-01-29 06:43:44 +1100

    [AD] adds a function to extract trees in newick form from the posterior sample

--- bin/test_dodonaphy.R
+++ bin/test_dodonaphy.R
@@ -6,6 +6,10 @@ library(phangorn)
 library(rstan)
 library(cmdstanr)
 
+# NOTE: it is necessary to set the path to cmdstan. uncomment the below
+# set the path appropriately for your system
+# set_cmdstan_path("~/software/cmdstan-2.21.0")
+
 dim <- 3    # number of dimensions for embedding
 nseqs <- 6  # number of sequences to simulate
 seqlen <- 1000  # length of sequences to simulate
@@ -48,11 +52,46 @@ encode_tipdata <- function(dnaseq){
 	tipdata
 }
 tipdata <- encode_tipdata(dnaseq)
-dphy_dat <- list(D=dim, L=seqlen, S=nseqs, tipdata=tipdata, leaf_locs=leaf_locs)
+dphy_dat <- list(D=dim+1, L=seqlen, S=nseqs, tipdata=tipdata, leaf_locs=leaf_locs)
 
 
 file.copy("src/dodonaphy_cpp_utils.hpp", paste(tempdir(),"/user_header.hpp", sep=""), overwrite=TRUE)
 dphy_mod <- cmdstan_model("src/dodonaphy.stan",stanc_options=list(allow_undefined=TRUE))
-dphy_mod$variational(data=dphy_dat)
+vbfit <- dphy_mod$variational(data=dphy_dat,tol_rel_obj=0.001,output_samples=1000)
+
+blens<-vbfit$draws("blens")
+peels<-vbfit$draws("peel")
+
+
+
+# extracts a newick format string that represents the tree
+tree_to_newick<-function(tip_names, peel_row, blen_row){
+	newick <- ""
+	chunks <- list()
+	plen <- length(tip_names)-1
+	for(p in 1:plen){
+		n1 <- peel_row[p]
+		n2 <- peel_row[p+plen]
+		n3 <- peel_row[p+2*plen]
+		if(n1 <= length(tip_names)){
+			chunks[n1] <- paste(tip_names[n1], ":", blen_row[n1], sep="")
+		}
+		if(n2 <= length(tip_names)){
+			chunks[n2] <- paste(tip_names[n2], ":", blen_row[n2], sep="")
+		}
+		n3name <- paste(as.character(n3), ":", blen_row[n3], sep="")
+		if(p == plen){
+			n3name <- ";"
+		}
+		chunks[n3] <- paste("(", chunks[n1], ",", chunks[n2], ")", n3name, sep="")
+	}
+	as.character(chunks[peel_row[length(peel_row)]])
+}
+
+testtree<-tree_to_newick(names(dnaseq), peels[30,], blens[30,])
+ttt1<-read.tree(text=as.character(testtree)) 
+plot(ttt1)
+add.scale.bar()
+
 
 
