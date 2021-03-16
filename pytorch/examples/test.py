c4426c1c8d4679563dcfcbe73d1e1f652e2bff40
Mathieu Fourment <m.fourment@gmail.com>
2021-03-17 10:06:43 +1100

    Allow test.py to take a fasta file and dimension as parameters

--- pytorch/examples/test.py
+++ pytorch/examples/test.py
@@ -1,21 +1,15 @@
 import sys
-import os
-import rdata
 
-# sys.path.append(os.path.abspath(os.path.join('../src')))
+from dendropy import DnaCharacterMatrix
+from dodonaphy.model import DodonaphyModel
+from dodonaphy.phylo import compress_alignment
 
-from model import DodonaphyModel
 
-def testFunc():
-    dat1 = rdata.conversion.convert(
-        rdata.parser.parse_file("/home/azad/UTS-RF-Projects/pyDodonaphy/dphy_data.rda")
-    )
-    dat2 = rdata.conversion.convert(
-        rdata.parser.parse_file("/home/azad/UTS-RF-Projects/pyDodonaphy/param_init.rda")
-    )
-    
-    mymod = DodonaphyModel(int(dat1['dphy_dat']['D']),  int(dat1['dphy_dat']['L']), int(dat1['dphy_dat']['S']))
-    mymod.learn(dat1['dphy_dat'], dat2['param_init'])
+def testFunc(alignment_path, dimension):
+    dna = DnaCharacterMatrix.get(path=alignment_path, schema='fasta')
+    partials, weights = compress_alignment(dna)
+    mymod = DodonaphyModel(partials, weights, dimension)
+    mymod.learn()
 
-    
-testFunc()
+
+testFunc(sys.argv[1], int(sys.argv[2]))
