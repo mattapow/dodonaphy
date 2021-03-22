4bde7935ae4e85e7d20368b1b4b95dbcc8ae35d6
mattapow <30282081+mattapow@users.noreply.github.com>
2021-03-22 16:55:07 +1100

    Hyrdra can return embedding stress
    Implemented extra keywords of Hydra.
    Now the only difference from CRAN version is that the R version can optionally get pairwise distance.

--- pytorch/dodonaphy/utils.py
+++ pytorch/dodonaphy/utils.py
@@ -4,7 +4,7 @@ from collections import deque
 import numpy as np
 import torch
 import warnings
-import sys
+
 
 class u_edge:
     def __init__(self, distance, node_1, node_2):
@@ -12,145 +12,207 @@ class u_edge:
         self.from_ = node_1
         self.to_ = node_2
 
+
 class utilFunc:
     def __init__(self):
         pass
 
     @staticmethod
-    def hydra(D, dim=2, curvature = 1, alpha=1.1, equi_adj=0.5, control={'lorentz': False}):
-        """ Strain minimised hyperbolic embedding
-        Python Implementation of Keller-Ressel's 2019 CRAN function hydra
+    def hydra(D, dim=2, curvature=1, alpha=1.1, equi_adj=0.5, **kwargs):
+        """Strain minimised hyperbolic embedding
+        Python Implementation of Martin Keller-Ressel's 2019 CRAN function
+        hydra
         https://arxiv.org/abs/1903.08977
-        
-        Args:
-            D (numpy matrix): pairwise distance matrix
-            dim (positive int): embedding dimension (default: 2)
-            curvature (positive real): embedding curvature (default: 1)
-            alpha (real greater than 1): adjusts the hyperbolic curvature. Values larger than one yield a more distorted embedding where points are pushed
-            to the outer boundary (i.e. the ideal points) of hyperblic space. The interaction between code{curvature} and code{alpha} is non-linear.
-            equi_adj (real): equi-angular adjustment; must be a real number between zero and one; only used if code{dim} is 2. Value 0 means no ajustment, 1 adjusts
-            embedded data points such that their angular coordinates in the Poincare disc are uniformly distributed. Other values interpolate between the two extremes.
-            Setting the parameter to non-zero values can make the embedding result look more harmoniuous in plots.
-        
-            control: a list which may contain the following boolean flags:
-                polar: return polar coordinates in dimension 2 (default: TRUE if code{dim} is 2. This flag is ignored in higher dimension)
-                isotropic_adj: perform isotropic adjustment, ignoring Eigenvalues (default: TRUE if code{dim} is 2, FALSE else)
-                lorentz: return raw Lorentz coordinates (before projection to hyperbolic space) (default: FALSE)
-                
-                (Not implemented)
-                stress: return embedding stress
-                dist: return hyperbolic distance matrix of embedded points (default: FALSE)
+
+        Parameters
+        ----------
+        D : ndarray
+            Pairwise distance matrix.
+        dim : Int, optional
+            Embedding dimension. The default is 2.
+        curvature : Float, optional
+            Embedding curvature. The default is 1.
+        alpha : Float, optional
+            Adjusts the hyperbolic curvature. Values larger than one yield a
+            more distorted embedding where points are pushed to the outer
+            boundary (i.e. the ideal points) of hyperblic space. The
+            interaction between code{curvature} and code{alpha} is non-linear.
+            The default is 1.1.
+        equi_adj : Float, optional
+            Equi-angular adjustment; must be a real number between zero and
+            one; only used if dim is 2. Value 0 means no ajustment, 1
+            adjusts embedded data points such that their angular coordinates
+            in the Poincare disc are uniformly distributed. Other values
+            interpolate between the two extremes. Setting the parameter to non-
+            zero values can make the embedding result look more harmoniuous in
+            plots. The default is 0.5.
+        **kwargs :
+            polar :
+                Return polar coordinates in dimension 2. This flag is
+                ignored in higher dimension).
+            isotropic_adj :
+                Perform isotropic adjustment, ignoring Eigenvalues
+                (default: TRUE if dim is 2, FALSE else)
+            lorentz :
+                Return raw Lorentz coordinates (before projection to
+                hyperbolic space) (default: FALSE)
+            stress :
+                Return embedding stress
+
+
+        Yields
+        ------
+        An dictionary with:
+            r : ndarray
+                1D array of the radii of the embeded points
+            direction : ndarray
+                dim-1 array of the directions of the embedded points
+            theta : ndarray
+                1D array of the polar coordinate angles of the embedded points
+                only if embedded into 2D Poincare disk
+            stress : float
+                The stress of the embedding
+
         """
 
-        
-        ## sanitize/check input
+        # sanitize/check input
         if(any(np.diag(D) != 0)):  # non-zero diagonal elements are set to zero
             np.fill_diagonal(D, 0)
             warnings.warn("Diagonal of input matrix D has been set to zero")
-  
+
         if not np.allclose(D, np.transpose(D)):
-            sys.exit("Input matrix is not symmetric.")
-            # warnings.warn("Input matrix D is not symmetric. Lower triangle part is used.")
-    
+            warnings.warn(
+                "Input matrix D is not symmetric.\
+                    Lower triangle part is used.")
+
         if dim == 2:
             # set default values in dimension 2
-            if "isotropic_adj" in control:
-                control.isotropic_adj = True
-            if "polar" in control:
-                control.polar = True
+            if "isotropic_adj" in kwargs:
+                kwargs.isotropic_adj = True
+            if "polar" in kwargs:
+                kwargs.polar = True
         else:
             # set default values in dimension > 2
-            if "isotropic_adj" in control:
-                control.isotropic_adj = False
-            if "polar" in control:
+            if "isotropic_adj" in kwargs:
+                kwargs.isotropic_adj = False
+            if "polar" in kwargs:
                 warnings.warn("Polar coordinates only valid in dimension two")
-                control.polar = False
+                kwargs.polar = False
             if equi_adj != 0.0:
-               warnings.warn("Equiangular adjustment only possible in dimension two.") 
+                warnings.warn(
+                    "Equiangular adjustment only possible in dimension two.")
 
-        A = np.cosh(np.sqrt(curvature)*D) # convert distance matrix to 'hyperbolic Gram matrix'
+        # convert distance matrix to 'hyperbolic Gram matrix'
+        A = np.cosh(np.sqrt(curvature)*D)
         n = A.shape[0]
-        
-        ## check for large/infinite values
+
+        # check for large/infinite values
         A_max = np.amax(A)
         if(A_max > 1e8):
-            warnings.warn("Gram Matrix contains values > 1e8. Rerun with smaller curvature parameter or rescaled distances.")
+            warnings.warn(
+                "Gram Matrix contains values > 1e8. Rerun with smaller\
+                curvature parameter or rescaled distances.")
         if A_max == float("inf"):
-            warnings.warn("Gram matrix contains infinite values. Rerun with smaller curvature parameter or rescaled distances.")
+            warnings.warn(
+                "Gram matrix contains infinite values.\
+                Rerun with smaller curvature parameter or rescaled distances.")
 
-        ## Compute Eigendecomposition of A
+        # Compute Eigendecomposition of A
         w, v = np.linalg.eigh(A)
-        idx = w.argsort()[::-1]   
+        idx = w.argsort()[::-1]
         w = w[idx]
-        v = v[:,idx]
-    
-        ## Extract leading Eigenvalue and Eigenvector
+        v = v[:, idx]
+
+        # Extract leading Eigenvalue and Eigenvector
         lambda0 = w[0]
-        x0 = v[:,0]
-    
-        ## Extract lower tail of spectrum)
-        X = v[:,(n-dim):n] # Last dim Eigenvectors
-        spec_tail = w[(n-dim):n] # Last dim Eigenvalues
+        x0 = v[:, 0]
+
+        # Extract lower tail of spectrum)
+        X = v[:, (n-dim):n]  # Last dim Eigenvectors
+        spec_tail = w[(n-dim):n]  # Last dim Eigenvalues
         # A_frob = np.sqrt(np.sum(v**2)) # Frobenius norm of A
 
-        x0 = x0 * np.sqrt(lambda0) # scale by Eigenvalue
-        if(x0[0]<0):
-            x0 = -x0 # Flip sign if first element negative
-        x_min = min(x0) # find minimum
+        x0 = x0 * np.sqrt(lambda0)  # scale by Eigenvalue
+        if(x0[0] < 0):
+            x0 = -x0  # Flip sign if first element negative
+        x_min = min(x0)  # find minimum
 
-        if not control.get('isotropic_adj'): # no isotropic adjustment: rescale Eigenvectors by Eigenvalues
-            if np.array([spec_tail>0]).any():
-                warnings.warn("Spectral Values have been truncated to zero. Try to use lower embedding dimension")
-                spec_tail[spec_tail>0] = 0
+        # no isotropic adjustment: rescale Eigenvectors by Eigenvalues
+        if not kwargs.get('isotropic_adj'):
+            if np.array([spec_tail > 0]).any():
+                warnings.warn(
+                    "Spectral Values have been truncated to zero. Try to use\
+                    lower embedding dimension")
+                spec_tail[spec_tail > 0] = 0
             X = np.matmul(X, np.diag(np.sqrt(-spec_tail)))
 
-        s = np.sqrt(np.sum(X**2,axis=1))        
-        directional = X / s[:,None] # convert to directional coordinates
-        
-        output = {} # Allocate output list
-        
-        ## Calculate radial coordinate
-        r = np.sqrt((alpha*x0 - x_min)/(alpha*x0 + x_min)) ## multiplicative adjustment (scaling)
+        s = np.sqrt(np.sum(X**2, axis=1))
+        directional = X / s[:, None]  # convert to directional coordinates
+
+        output = {}  # Allocate output list
+
+        # Calculate radial coordinate
+        # multiplicative adjustment (scaling)
+        r = np.sqrt((alpha*x0 - x_min)/(alpha*x0 + x_min))
         output['r'] = r
-        
-        ## Calculate polar coordinates if dimension is 2
-        if(dim==2):
-          ## calculate polar angle
-          theta = np.arctan2(X[:,0],-X[:,1])
-          
-          ## Equiangular adjustment
-          if equi_adj > 0.0:
-            angles = [(2*x/n-1)*math.pi for x in range(0, n)]
-            theta_equi = np.array([x for _,x in sorted(zip(theta,angles))]) # Equi-spaced angles
-            theta = (1-equi_adj)*theta + equi_adj*theta_equi # convex combination of original and equi-spaced angles
-            directional = np.array([np.cos(theta),np.sin(theta)]).transpose() # update directional coordinate
-         
-            output['theta'] = theta
-        
+
+        # Calculate polar coordinates if dimension is 2
+        if(dim == 2):
+            # calculate polar angle
+            theta = np.arctan2(X[:, 0], -X[:, 1])
+
+            # Equiangular adjustment
+            if equi_adj > 0.0:
+                angles = [(2*x/n-1)*math.pi for x in range(0, n)]
+                theta_equi = np.array([x for _, x in sorted(
+                    zip(theta, angles))])  # Equi-spaced angles
+                # convex combination of original and equi-spaced angles
+                theta = (1-equi_adj)*theta + equi_adj*theta_equi
+                # update directional coordinate
+                directional = np.array(
+                    [np.cos(theta), np.sin(theta)]).transpose()
+
+                output['theta'] = theta
+
         output['directional'] = directional
 
-        ## Set Additional return values
-        if control.get('lorentz'):
-          output['x0'] = x0
-          output['X'] = X
-          
-        #out$strain.R2 <- (lambda0^2 + sum(pmax(-spec.tail,0)^2)) / A.frob^2 # R^2 of residual strain
-        
-        if control.get('dist'): ## Calculate hyperbolic distances (can be time consuming)
-            sys.exit("get.distance and get.stress functions not implemented yet")
-            # out.dist = get.distance(r,directional,curvature,D)
-            # out.stress <- attr(out$dist,"stress")
-            # attr(out.dist,"stress") <- NULL
+        # Set Additional return values
+        if kwargs.get('lorentz'):
+            output['x0'] = x0
+            output['X'] = X
 
+        if kwargs.get('stress'):
+            output['stress'] = utilFunc.stress(r, directional, curvature, D)
 
-        if control.get('stress') and not control.get('dist'):
-            sys.exit("get.distance and get.stress functions not implemented yet")
-            # out.stress = get.stress(r,directional,curvature,D) ## Calculate stress only
-        
         output['curvature'] = curvature
         output['dim'] = dim
         return(output)
-        
+
+    @staticmethod
+    def stress(r, directional, curvature, D):
+        # Calculate stress of embedding from radial/directional coordinate
+        # From Nagano 2019
+        n = len(r)  # number of embeded points
+        stress_sq = 0.0  # allocate squared stress
+
+        # convert from numpy to torch
+        dist = torch.zeros((n, n))
+        r = torch.from_numpy(r)
+        directional = torch.from_numpy(directional)
+        curvature = torch.tensor(curvature, dtype=torch.double)
+        D = torch.tensor(D)
+
+        for i in range(n):
+            for j in range(n):
+                if i != j:
+                    dist[i][j] = utilFunc.hyperbolic_distance(
+                        r[i], r[j],
+                        directional[i, ], directional[j, ],
+                        curvature)
+                    stress_sq = stress_sq + (dist[i][j] - D[i, j]) ** 2
+
+        return np.sqrt(stress_sq)
+
     @staticmethod
     def hyperbolic_distance(r1, r2, directional1, directional2, curvature):
         """Generates hyperbolic distance between two points in poincoire ball
--- pytorch/test/test_utils.py
+++ pytorch/test/test_utils.py
@@ -1,11 +1,75 @@
 import pytest
 import torch
+import numpy as np
+from pytest import approx
 
 from dodonaphy.utils import utilFunc
 
 
 def test_hyperbolic_distance():
-    dist = utilFunc.hyperbolic_distance(torch.tensor([0.5]), torch.tensor([0.6]), torch.tensor([0.1, 0.3]),
-                                        torch.tensor([0.5, 0.5]),
-                                        torch.tensor([1.]))
+    dist = utilFunc.hyperbolic_distance(
+        torch.tensor([0.5]), torch.tensor([0.6]), torch.tensor([0.1, 0.3]),
+        torch.tensor([0.5, 0.5]), torch.tensor([1.]))
     assert 1.777365 == pytest.approx(dist.item(), 0.0001)
+
+
+def test_hydra_2d_compare_output():
+
+    D = np.ones((3, 3), float)
+    np.fill_diagonal(D, 0.0)
+    dim = 2
+    emm = utilFunc.hydra(D, dim)
+
+    # Compare to output of hydra in r
+    assert emm['curvature'] == approx(1)
+    assert emm['dim'] == approx(dim)
+    assert emm['directional'] == approx(np.array([
+        [0.8660254038, -0.5000000000],
+        [-0.8660254038, -0.5000000000],
+        [0.0000000000, 1.0000000000]
+    ]))
+    assert emm['r'] == approx(
+        np.array([0.2182178902, 0.2182178902, 0.2182178902]))
+    assert emm['theta'] == approx(
+        np.array([-0.5235987756, -2.6179938780, 1.5707963268]))
+
+
+def test_hydra_3d_compare_output():
+
+    D = np.array([[0, 1, 2.5, 3, 1], [1, 0, 2.2, 1, 2], [
+                 2.5, 2.2, 0, 3, 1], [3, 1, 3, 0, 2], [1, 2, 1, 2, 0]])
+    dim = 3
+    emm = utilFunc.hydra(D, dim, equi_adj=0)
+
+    # Compare to output of hydra in r
+    # NB: some directions reversed from r due to opposite eigenvectors
+    assert emm['curvature'] == approx(1)
+    assert emm['dim'] == approx(dim)
+    assert emm['r'] == approx(np.array(
+        [0.6274604254, 0.2705702432, 0.6461609880, 0.6779027826, 0.2182178902])
+    )
+    assert emm['directional'] == approx(np.array([
+        [0.0821320214, -0.7981301820, 0.5968605730],
+        [-0.5711268681, -0.6535591501, -0.4966634050],
+        [-0.2070516064, 0.6620783581, 0.7202651457],
+        [0.0811501819, 0.1418186867, -0.9865607473],
+        [0.8008637619, 0.2731045145, 0.5329457375]
+    ]))
+
+
+def test_hydra_stress_2d():
+    D = np.random.rand(5, 5)
+    D = D + np.transpose(D)
+    np.fill_diagonal(D, 0.0)
+    dim = 2
+
+    utilFunc.hydra(D, dim, lorentz=False, stress=True)
+
+
+def test_hydra_lorentz_2d():
+    D = np.random.rand(4, 4)
+    D = D + np.transpose(D)
+    np.fill_diagonal(D, 0.0)
+    dim = 2
+
+    utilFunc.hydra(D, dim, lorentz=True, stress=True)
