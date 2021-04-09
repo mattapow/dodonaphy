be3ec4c98aeccf45f51250679174941f7e7b19f4
mattapow <30282081+mattapow@users.noreply.github.com>
2021-04-09 13:00:51 +1000

    add test_draws_different()

--- pytorch/test/test_model.py
+++ pytorch/test/test_model.py
@@ -73,6 +73,36 @@ def test_model_init_rand():
     plt.show()
 
 
+def test_draws_different():
+    """
+    Each draw from the sample should be different in likelihood.
+
+    """
+    dim = 2  # number of dimensions for embedding
+    nseqs = 6  # number of sequences to simulate
+    seqlen = 1000  # length of sequences to simulate
+
+    # # simulate a tree
+    simtree = treesim.birth_death_tree(
+        birth_rate=1.0, death_rate=0.5, num_extant_tips=nseqs)
+    dna = simulate_discrete_chars(
+        seq_len=seqlen, tree_model=simtree, seq_model=dendropy.model.discrete.Jc69())
+
+    # Initialise model
+    partials, weights = compress_alignment(dna)
+    mymod = DodonaphyModel(partials, weights, dim)
+
+    # learn
+    mymod.learn(epochs=0)
+
+    # draw
+    nsamples = 3
+    peels, blens, X, lp__ = mymod.draw_sample(nsamples, lp=True)
+    assert not torch.equal(lp__[0], lp__[1])
+    assert not torch.equal(lp__[0], lp__[2])
+    assert not torch.equal(lp__[1], lp__[2])
+
+
 def test_init_RAxML_hydra():
     """
     Initialise the emebedding with RAxML distances given to hydra.
