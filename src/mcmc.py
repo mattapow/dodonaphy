5953acdaca8c91d8bd4a291f8c13dd9cc67b2e18
mattapow <30282081+mattapow@users.noreply.github.com>
2021-05-31 08:49:02 +1000

    Contained MCMC class. All np to torch
    Should have it's own method implementations of  compute_branch_lengths and compute LL because it isn't really a child of class Model which is for VI.

--- src/mcmc.py
+++ src/mcmc.py
@@ -1,24 +1,26 @@
-from .model import DodonaphyModel
 from .utils import utilFunc
 from .hyperboloid import t02p
-from numpy.random import normal, uniform
-import numpy as np
-import os
+from .phylo import calculate_treelikelihood, JC69_p_t
+from torch.distributions import normal, uniform
+import torch
 import matplotlib.pyplot as plt
 import matplotlib.cm
 
 
-class Mcmc(DodonaphyModel):
+class Mcmc(object):
 
     def __init__(self, partials, weights, D, loc):
-        DodonaphyModel.__init__(self, partials, weights, D)
+        self.partials = partials
+        self.weights = weights
+        self.D = D
         self.loc = loc  # in tangent space t_0
+
+        self.S = len(partials)
+        self.L = partials[0].shape[1]
+        self.bcount = 2 * self.S - 2
 
     def learn(self, n_steps, burnin=0, path_write='./out', save_period=1, step_scale=0.01):
         self.step_scale = step_scale
-
-        # os.makedirs(path_write, exist_ok=True)
-        os.mkdir(path_write)
         self.save_period = save_period
 
         fn = path_write + '/' + 'mcmc.info'
@@ -38,16 +40,17 @@ class Mcmc(DodonaphyModel):
         if self.D == 2:
             _, ax = plt.subplots(1, 1, sharex=True, sharey=True)
             cmap = matplotlib.cm.get_cmap('plasma')
+
         for i in range(n_steps):
             # step
             accepted += self.step()
 
             # set peel + blens + poincare locations
-            loc_poin = t02p(self.loc, np.zeros_like(self.loc), self.D)
+            loc_poin = t02p(self.loc, torch.zeros_like(self.loc), self.D)
             leaf_r, int_r, leaf_dir, int_dir = utilFunc.cart_to_dir_tree(loc_poin)
             self.peel = utilFunc.make_peel(leaf_r, leaf_dir, int_r, int_dir)
             self.blens = super().compute_branch_lengths(self.S, self.D, self.peel, leaf_r, leaf_dir, int_r, int_dir)
-            loc_poin = np.concatenate((loc_poin, np.expand_dims(loc_poin[0, :], axis=0)))
+            loc_poin = torch.cat((loc_poin, torch.unsqueeze(loc_poin[0, :], axis=0)))
 
             # plot
             if self.S == 2:
@@ -65,14 +68,14 @@ class Mcmc(DodonaphyModel):
             plt.show()
 
     def step(self):
-        loc_proposal = self.loc + normal(0, self.step_scale, size=(2*self.S - 2, self.D))
+        loc_proposal = self.loc + normal.Normal(0, self.step_scale, size=(2*self.S - 2, self.D))
         r = self.accept_ratio(loc_proposal)
         # TODO: maybe move each node one at a time?
 
         accept = False
         if r >= 1:
             accept = True
-        elif uniform() < r:
+        elif uniform.Uniform(torch.zeros(1), torch.ones(1)) < r:
             accept = True
         if accept:
             self.loc = loc_proposal
@@ -83,7 +86,7 @@ class Mcmc(DodonaphyModel):
         like_current = self.compute_LL(leaf_r, leaf_dir, int_r, int_dir).detach().numpy()
         leaf_r, int_r, leaf_dir, int_dir = utilFunc.cart_to_dir_tree(loc_proposal)
         like_proposal = self.compute_LL(leaf_r, leaf_dir, int_r, int_dir).detach().numpy()
-        like_ratio = np.exp(like_proposal - like_current)
+        like_ratio = torch.exp(like_proposal - like_current)
 
         # TODO: priors?
         # log(distToOrigin)
@@ -94,4 +97,64 @@ class Mcmc(DodonaphyModel):
         # Proposals are symmetric Guassians
         hastings_ratio = 1
 
-        return np.minimum(1., prior_ratio * like_ratio * hastings_ratio)
+        return torch.minimum(1., prior_ratio * like_ratio * hastings_ratio)
+
+    def compute_branch_lengths(self, S, dim, peel, leaf_r, leaf_dir, int_r, int_dir, curvature=1):
+        """Computes the hyperbolic distance of two points given in radial/directional coordinates in the Poincare ball
+
+        Args:
+            S (integer): [description]
+            D ([type]): [description]
+            peel ([type]): [description]
+            leaf_r ([type]): [description]
+            leaf_dir ([type]): [description]
+            int_r ([type]): [description]
+            int_dir ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        blens = torch.empty(self.bcount, dtype=torch.float64)
+        for b in range(S-1):
+            directional2 = int_dir[peel[b][2]-S-1, ]
+            r2 = int_r[peel[b][2]-S-1]
+
+            for i in range(2):
+                if peel[b][i] < S:
+                    # leaf to internal
+                    r1 = leaf_r[peel[b][i]]
+                    directional1 = leaf_dir[peel[b][i], :]
+                else:
+                    # internal to internal
+                    r1 = int_r[peel[b][i]-S-1]
+                    directional1 = int_dir[peel[b][i]-S-1, ]
+
+                hd = utilFunc.hyperbolic_distance(
+                    r1, r2, directional1, directional2, curvature)
+
+                # apply the inverse transform from Matsumoto et al 2020
+                hd = torch.log(torch.cosh(hd))
+
+                # add a tiny amount to avoid zero-length branches
+                eps = torch.finfo(torch.double).eps
+                blens[peel[b][i]] = torch.maximum(hd, eps)
+
+        return blens
+
+    def compute_LL(self, leaf_r, leaf_dir, int_r, int_dir):
+        """[summary]
+
+        Args:
+            leaf_r ([type]): [description]
+            leaf_dir ([type]): [description]
+            int_r ([type]): [description]
+            int_dir ([type]): [description]
+        """
+
+        peel = utilFunc.make_peel(leaf_r, leaf_dir, int_r, int_dir)
+
+        # branch lengths
+        blens = self.compute_branch_lengths(self.S, self.D, peel, leaf_r, leaf_dir, int_r, int_dir)
+
+        mats = JC69_p_t(blens)
+        return calculate_treelikelihood(self.partials, self.weights, peel, mats, torch.full([4], 0.25, dtype=torch.float64))
