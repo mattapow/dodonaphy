b5f59d5c2ce305d6c2be463431f8b50144205b62
mattapow <30282081+mattapow@users.noreply.github.com>
2021-06-02 16:15:25 +1000

    Choose best of initial random int_node
    Internal nodes randomly placed by exponential distributions. The starting locations with the best log-likelihood is used.

--- main_mcmc.py
+++ main_mcmc.py
@@ -12,6 +12,7 @@ import torch
 import random
 from dendropy.interop import raxml
 import os
+import math
 
 
 def main():
@@ -23,6 +24,8 @@ def main():
     rng = random.Random(1)
     simtree = treesim.birth_death_tree(birth_rate=2., death_rate=0.5, num_extant_tips=S, rng=rng)
     dna = simulate_discrete_chars(seq_len=seqlen, tree_model=simtree, seq_model=dendropy.model.discrete.Jc69())
+    # compress alignment
+    partials, weights = compress_alignment(dna)
 
     # save dna to nexus
     path_write = "./out"
@@ -54,11 +57,9 @@ def main():
         print('Embedding Stress (tips only) = {:.4}'.format(emm["stress"].item()))
 
         # internal nodes near origin
-        scale = .1 * emm["r"].min()
-        emm["r"] = np.concatenate((emm["r"], np.random.exponential(scale=scale, size=(S-2))))
-        u = np.random.normal(0, 1, (S-2, dim))
-        norm = np.sum(u**2, axis=1)**0.5
-        emm["directional"] = np.concatenate((emm["directional"], u/norm.reshape(S-2, 1)))
+        int_r, int_dir = initialise(emm, dim, partials, weights)
+        emm["r"] = np.concatenate((emm["r"], int_r))
+        emm["directional"] = np.concatenate((emm["directional"], int_dir))
 
     # store in tangent plane R^dim
     loc_poin = utilFunc.dir_to_cart(torch.from_numpy(emm["r"]), torch.from_numpy(emm["directional"]))
@@ -72,9 +73,45 @@ def main():
     epochs = 1000
     mymod.learn(epochs, path_write=path_write, step_scale=0.01, save_period=1)
 
-    # path = './out/mcmc.tree'
-    # treelist = dendropy.TreeList.get(path=path, schema='newick')
-    # for tree in trees:
+
+def initialise(emm_tips, dim, partials, weights):
+    # try out some inner node positions and pick the best
+    print("Randomly initialising internal node positions.")
+
+    S = len(emm_tips['r'])
+    scale = torch.as_tensor(.5 * emm_tips['r'].min())
+    mod = Mcmc(partials, weights, dim, None)
+    lnP = -math.inf
+
+    dir = np.random.normal(0, 1, (S-2, dim))
+    abs = np.sum(dir**2, axis=1)**0.5
+    _int_r = np.random.exponential(scale=scale, size=(S-2))
+    _int_dir = dir/abs.reshape(S-2, 1)
+
+    n_scale = 10
+    max_scale = 5 * emm_tips['r'].min()
+    n_trials = 100
+    for i in range(n_scale):
+        _scale = torch.as_tensor((i+1)/(n_scale+1) * max_scale)
+        for _ in range(n_trials):
+            _lnP = mod.compute_LL(
+                torch.from_numpy(emm_tips['r']), torch.from_numpy(emm_tips['directional']),
+                torch.from_numpy(_int_r), torch.from_numpy(_int_dir))
+
+            if _lnP > lnP:
+                int_r = _int_r
+                int_dir = _int_dir
+                lnP = _lnP
+                scale = _scale
+
+            dir = np.random.normal(0, 1, (S-2, dim))
+            abs = np.sum(dir**2, axis=1)**0.5
+            _int_r = np.random.exponential(scale=_scale, size=(S-2))
+            _int_dir = dir/abs.reshape(S-2, 1)
+
+    print("Best internal node positions found.")
+
+    return int_r, int_dir
 
 
 if __name__ == '__main__':
