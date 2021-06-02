c931709db9814980130b49ffd92b8ca80d4c0b02
mattapow <30282081+mattapow@users.noreply.github.com>
2021-06-02 12:35:43 +1000

    Update stat_cmp.py

--- stat_cmp.py
+++ stat_cmp.py
@@ -1,28 +1,35 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-# NB: pandas requires scipy
+import dendropy
 
 
 def stat_cmp():
     """
-    Compare statistics in output of TreeStat
+    Compare Dodonaphy to Beast outputs
+
+    First run Dodonaphy and Beast to obtain output nexus files containing Newick trees
+    Prints the consensus tree and the Maximum Clade Credibility Tree
     """
 
-    dir = "../outs/S10_D2_STEP03_EPOCH1000/Int_all_hydra"
-    fn1 = dir + "/mcmc_stat.txt"
-    fn2 = dir + "/beast_stat.txt"
+    dir = "./out"
+    fn1 = dir + "/mcmc.trees"
+    fn2 = dir + "/beast.trees"
 
-    df1 = pd.read_csv(fn1, delimiter='\t', header=0, index_col='state')
-    df2 = pd.read_csv(fn2, delimiter='\t', header=0, index_col='state')
+    dodo_trees = dendropy.TreeList.get(path=fn1, schema="nexus")
+    bst_trees = dendropy.TreeList.get(path=fn2, schema="nexus")
 
-    assert all(df1.columns == df2.columns)
+    dodo_splits = dodo_trees.split_distribution()
+    bst_splits = bst_trees.split_distribution()
 
-    for name in df1.columns:
-        df1[name].plot.kde()
-        df2[name].plot.kde()
-        plt.xlabel(name)
-        plt.legend(["Dodonaphy", "Beast"])
-        plt.show()
+    # print consensus trees
+    print("Dodonaphy Consensus Tree:")
+    dodo_splits.consensus_tree().print_plot()
+    print("BEAST Consensus Tree:")
+    bst_splits.consensus_tree().print_plot()
+
+    # print maxmim clade credibility tree
+    print("Dodonaphy Maximim Clade Credibility Set:")
+    dodo_trees.maximum_product_of_split_support_tree().print_plot()
+    print("BEAST Maximim Clade Credibility Set:")
+    bst_trees.maximum_product_of_split_support_tree().print_plot()
 
 
 stat_cmp()
