2021cc0b1f14eb93acabc985233f5a39e8bfa2b0
mattapow <30282081+mattapow@users.noreply.github.com>
2021-08-19 16:16:52 +1000

    Second prior test

--- dodonaphy/test/test_phylo.py
+++ dodonaphy/test/test_phylo.py
@@ -38,7 +38,7 @@ def test_calculate_likelihood():
                                  torch.full([4], 0.25, dtype=torch.float64))
 
 
-def test_prior_mrbayes():
+def test_prior_mrbayes_0():
     data = "(6:2.000000e-02,((5:2.000000e-02,2:2.000000e-02):2.000000e-02,\
         (4:2.000000e-02,3:2.000000e-02):2.000000e-02):2.000000e-02,1:2.000000e-02);"
     dendo_tree = dendropy.Tree.get(data=data, schema="newick")
@@ -47,3 +47,14 @@ def test_prior_mrbayes():
     lnPrior = BaseModel.compute_prior_gamma_dir(blens, aT=torch.ones(1), bT=torch.full((1,), .1), a=torch.ones(1),
                                                 c=torch.ones(1))
     assert lnPrior == approx(17.34844, abs=1e-5)
+
+
+def test_prior_mrbayes_1():
+    data = "((2:1.179257e-01,(6:1.047047e-03,3:1.426334e-03):7.713732e-02):\
+        7.894008e-02,(4:2.848264e-03,5:2.711671e-03):1.879317e-03,1:4.419083e-03);"
+    dendo_tree = dendropy.Tree.get(data=data, schema="newick")
+    _, blens = treeFunc.dendrophy_to_pb(dendo_tree)
+
+    lnPrior = BaseModel.compute_prior_gamma_dir(blens, aT=torch.ones(1), bT=torch.full((1,), .1), a=torch.ones(1),
+                                                c=torch.ones(1))
+    assert lnPrior == approx(13.56829, abs=1e-5)
