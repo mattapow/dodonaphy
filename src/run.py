84a2d1a9109d8c9c3a75c868214a3d6e507201e9
mattapow <30282081+mattapow@users.noreply.github.com>
2021-09-22 10:13:01 +1000

    Update run.py

--- src/run.py
+++ src/run.py
@@ -19,28 +19,28 @@ def main():
         print("Usage: %s value" % sys.argv[0])
         sys.exit()
     try:
-        input = float(sys.argv[1])
+        input_dim = float(sys.argv[1])
     except Exception:
         print("Argument not a float.")
         sys.exit()
 
     # Simulation parameters
-    dim = int(input)  # number of dimensions for embedding
+    dim = int(input_dim)  # number of dimensions for embedding
     S = 17  # number of sequences to simulate
     L = 1000  # length of sequences to simulate
     prior = {"birth_rate": 2., "death_rate": .5}
-    epochs = 10000      # number of epochs
+    epochs = 500000      # number of epochs
     n_draws = 1000      # number of trees drawn from final distribution
     connect_method = 'nj'      # 'incentre', 'mst', 'geodesics', 'nj', 'mst_choice'
-    embed_method = 'simple'     # 'simple' or 'wrap'
+    embed_method = 'simple'    # 'simple' or 'wrap'
     doSave = True
-    inference = 'vi'
+    inference = 'mcmc'
     curvature = -1.
-    start_tree = 'RAxML'       # 'RAxML' or 'true_tree'
+    start_tree = 'true_tree'       # 'RAxML' or 'true_tree'
 
     # VI parameters
-    k_samples = 10       # tree samples per elbo calculation
-    lr = 1e-3
+    k_samples = 1       # tree samples per elbo calculation
+    lr = 1e-1
 
     # MCMC parameters
     step_scale = .001
@@ -49,42 +49,43 @@ def main():
     burnin = 0
 
     # MST parameters
-    n_trials = 100      # number of initial embeddings to select from per grid for mst
-    n_grids = 100       # number grid scales for selecting inital embedding for mst
+    n_trials = 10      # number of initial embeddings to select from per grid for mst
+    n_grids = 10       # number grid scales for selecting inital embedding for mst
     max_scale = 1       # for mst internal node positions
 
     # Experiment folder
-    path_write = "../data/T%d" % (S)
-    treePath = "%s/simtree.nex" % path_write
-    treeInfoPath = "%s/simtree.info" % path_write
-    dnaPath = "%s/dna.nex" % path_write
+    root_dir = os.path.abspath(os.path.join("../data", "T%d" % (S)))
+    tree_path = os.path.join(root_dir, "simtree.nex")
+    tree_info_path = os.path.join(root_dir, "simtree.info")
+    dna_path = os.path.join(root_dir, "dna.nex")
+    exp_method = "%s_%s" % (embed_method, connect_method)
 
     if inference == 'vi':
         if doSave:
             lnLr = -int(np.log10(lr))
-            path_write_vi = os.path.abspath(os.path.join(
-                path_write, "vi", "%s_%s_lr%i_k%i_d%i_crv%d" % (embed_method, connect_method, lnLr, k_samples, dim, curvature)))
+            method_dir = os.path.join(root_dir, "vi", exp_method)
+            path_write = os.path.join(
+                method_dir, "d%i_lr%i_k%i_crv%d" %
+                (dim, lnLr, k_samples, -curvature*100))
         else:
-            path_write_vi = None
-        runVi = True
-        runMcmc = False
+            path_write = None
 
     elif inference == 'mcmc':
         if doSave:
-            path_write_mcmc = os.path.abspath(os.path.join(
-                path_write, "mcmc", "%s_%s_c%d_d%d_crv%d" % (embed_method, connect_method, nChains, dim, curvature)))
+            method_dir = os.path.join(root_dir, "mcmc", exp_method)
+            path_write = os.path.join(
+                method_dir, "d%d_c%d_crv%d" %
+                (dim, nChains, -curvature*100))
         else:
-            path_write_mcmc = None
-        runMcmc = True
-        runVi = False
+            path_write = None
 
     try:
         # Try loading in the simTree and dna
-        simtree = dendropy.Tree.get(path=treePath, schema="nexus")
-        dna = dendropy.DnaCharacterMatrix.get(path=dnaPath, schema="nexus")
+        simtree = dendropy.Tree.get(path=tree_path, schema="nexus")
+        dna = dendropy.DnaCharacterMatrix.get(path=dna_path, schema="nexus")
     except (FileExistsError, FileNotFoundError):
         # Make experiment folder
-        os.makedirs(path_write, exist_ok=False)
+        os.makedirs(root_dir, exist_ok=False)
 
         # simulate a tree
         rng = random.Random(1)
@@ -93,14 +94,14 @@ def main():
         dna = simulate_discrete_chars(seq_len=L, tree_model=simtree, seq_model=dendropy.model.discrete.Jc69(), rng=rng)
 
         # save simtree
-        simtree.write(path=treePath, schema="nexus")
+        simtree.write(path=tree_path, schema="nexus")
 
         # save dna to nexus
-        dna.write_to_path(dest=dnaPath, schema="nexus")
+        dna.write_to_path(dest=dna_path, schema="nexus")
 
         # save simTree info log-likelihood
         LL = birth_death_likelihood(tree=simtree, birth_rate=prior['birth_rate'], death_rate=prior['death_rate'])
-        with open(treeInfoPath, 'w') as f:
+        with open(tree_info_path, 'w') as f:
             f.write('Log Likelihood: %f\n' % LL)
             simtree.write_ascii_plot(f)
 
@@ -122,35 +123,31 @@ def main():
             dists[i][i+j+1] = pdc(t1, t2)
     dists = dists + dists.transpose()
 
+    if path_write is not None:
+        if not os.path.exists(method_dir):
+            os.makedirs(method_dir, exist_ok=False)
+        os.mkdir(path_write)
+
     start = time.time()
-    if runMcmc:
+    if inference == 'mcmc':
         # Run Dodoanphy MCMC
-        if path_write_mcmc is not None:
-            os.mkdir(path_write_mcmc)
-        mcmc.run(dim, partials[:], weights, dists, path_write_mcmc,
+        mcmc.run(dim, partials[:], weights, dists, path_write,
                  epochs=epochs, step_scale=step_scale, save_period=save_period,
                  n_grids=n_grids, n_trials=n_trials, max_scale=max_scale, nChains=nChains,
                  burnin=burnin, connect_method=connect_method, embed_method=embed_method,
                  curvature=curvature, **prior)
 
-    if runVi:
+    if inference == 'vi':
         # Run Dodonaphy variational inference
-        if path_write_vi is not None:
-            os.mkdir(path_write_vi)
-        DodonaphyVI.run(dim, S, partials[:], weights, dists, path_write_vi,
+        DodonaphyVI.run(dim, S, partials[:], weights, dists, path_write,
                         epochs=epochs, k_samples=k_samples, n_draws=n_draws,
                         n_grids=n_grids, n_trials=n_trials,
                         max_scale=max_scale, lr=lr, embed_method=embed_method,
                         connect_method=connect_method, curvature=curvature, **prior)
-
     end = time.time()
     seconds = end-start
     m, s = divmod(seconds, 60)
     print("Time taken for %d taxa: %dm %ds" % (S, m, s))
-
-    # Make folder for BEAST
-    # path_write_beast = os.path.abspath(os.path.join(path_write, "beast"))
-    # os.mkdir(path_write_beast)
 
 
 if __name__ == "__main__":
