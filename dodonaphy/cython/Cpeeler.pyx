7ceb45936fb6013314095f8aacb4e002b4e4fcab
mattapow <matthew.macaulay@uts.edu.au>
2022-04-22 09:01:49 +1000

    Faster old NJ
    Uses a Q matrix.
    Still 4x slower than using a dictionary on DS1.

--- dodonaphy/cython/Cpeeler.pyx
+++ dodonaphy/cython/Cpeeler.pyx
@@ -126,6 +126,11 @@ def nj_np(double[:, ::1] pdm):
 
 
 cpdef nj_np_old(np.ndarray[np.double_t, ndim=2] pdm):
+    """Neighbour joining using Q matrix array (slower).
+    
+    Args:
+        pdm (ndarray): Pairwise distance matrix
+    """
     cdef np.int_t leaf_node_count = len(pdm)
     cdef np.int_t node_count = 2 * leaf_node_count - 2
     cdef np.double_t eps = np.double(2.220446049250313e-16)
@@ -133,8 +138,8 @@ cpdef nj_np_old(np.ndarray[np.double_t, ndim=2] pdm):
     cdef np.ndarray[np.int_t, ndim=2] peel = np.zeros((leaf_node_count - 1, 3), dtype=int)
     cdef np.ndarray[np.double_t, ndim=1] blens = np.zeros(node_count, dtype=np.double)
 
-    cdef np.ndarray[np.uint8_t, ndim=1] mask = np.array(leaf_node_count * [False])
-    cdef np.ndarray[np.int_t, ndim=1] node_map = np.arange(leaf_node_count)
+    cdef np.ndarray[np.uint8_t, ndim=1] mask = np.zeros(leaf_node_count, dtype=bool)
+    cdef int[:] node_map = np.arange(leaf_node_count, dtype=np.int32)
 
     cdef np.int_t left
     cdef np.int_t right
@@ -143,14 +148,14 @@ cpdef nj_np_old(np.ndarray[np.double_t, ndim=2] pdm):
     cdef np.ndarray[np.double_t, ndim=1] dist_p
     cdef np.double_t dist_pl
     cdef np.double_t dist_lr
-    cdef np.int_t n = pdm.shape[0]
-    cdef np.int_t m = pdm.shape[1]
+    cdef np.double_t n = pdm.shape[0]
+    cdef np.double_t m = pdm.shape[1]
     cdef np.ndarray[np.double_t, ndim=2] Q = np.zeros_like(pdm)
 
     for int_i in range(leaf_node_count - 1):
-        Q = compute_Q(pdm, mask)
-        left, right = unravel_index(Q.argmin(), (n, m))
-        dist_p = get_new_dist(pdm, mask, left, right)
+        Q, sum_pdm = update_Q(pdm, Q, mask, leaf_node_count)
+        left, right = get_argmin(Q, mask, leaf_node_count)
+        dist_p = get_new_dist(pdm, mask, left, right, sum_pdm)
         dist_pl = dist_p[left]
         dist_lr = pdm[left][right]
 
@@ -163,76 +168,69 @@ cpdef nj_np_old(np.ndarray[np.double_t, ndim=2] pdm):
         blens[node_map[right]] = np.maximum(dist_lr - dist_pl, eps)
 
         # replace right by dist_p in the pdm
-        pdm = np.concatenate((pdm[:right, :], np.expand_dims(dist_p, 0), pdm[right + 1 :, :]), axis=0)
-        pdm = np.concatenate(
-            (pdm[:, :right], np.expand_dims(dist_p, -1), pdm[:, right + 1 :]), axis=1
-        )
+        pdm[right, :] = dist_p
+        pdm[:, right] = dist_p
         node_map[right] = parent
         mask[left] = True
 
     return peel, blens
-
-
-cdef compute_Q(
+ 
+cdef get_argmin(np.ndarray[np.double_t, ndim=2] Q, np.ndarray[np.uint8_t, ndim=1] mask, np.int_t leaf_node_count):
+    cdef int i, j, min_i, min_j = 0
+    cdef np.double_t q_min = infty
+    for i in range(leaf_node_count):
+        for j in range(leaf_node_count):
+            valid = ~mask[i] and ~mask[j]
+            if i < j and valid and Q[i, j] < q_min:
+                q_min = Q[i, j]
+                min_i, min_j = i, j
+    return min_i, min_j
+ 
+cdef update_Q(
     np.ndarray[np.double_t, ndim=2] pdm,
-    mask=0,
-    fill_value=None):
+    np.ndarray[np.double_t, ndim=2] Q,
+    np.ndarray[np.uint8_t, ndim=1] mask,
+    np.int_t leaf_node_count,
+    ):
     """Compute the Q matrix for Neighbour joining.
 
     Args:
-        pdm (ndarray): [description]
-        n_active (long): [description]
+        pdm (ndarray): Pairwise distance
 
     Returns:
         [type]: [description]
     """
-    cdef np.int_t n_pdm = len(pdm)
-    if mask is False:
-        mask = np.full((n_pdm,), False)
-    cdef np.int_t n_active = np.sum(~mask)
-
     cdef np.ndarray[np.uint8_t, ndim=1] mask_invert = np.invert(mask)
+    cdef np.int_t n_active = np.sum(mask_invert)
     cdef np.ndarray[np.uint8_t, ndim=2] mask_2d = np.invert(np.outer(mask_invert, mask_invert))
-    cdef np.ndarray[np.double_t, ndim=2] sum_pdm = np.sum(pdm * ~mask_2d, axis=1, keepdims=True)
-    cdef np.ndarray[np.double_t, ndim=2] sum_i = np.tile(sum_pdm, (1, n_pdm))
-    cdef np.ndarray[np.double_t, ndim=2] sum_j = np.tile(sum_pdm.T, (n_pdm, 1))
-    Q = np.ma.array(
-        (n_active - 2) * pdm - sum_i - sum_j,
-        mask=mask_2d
-        )
-
-    if fill_value is None:
-        fill_value = np.finfo(np.double).max / 2.0
-    np.ma.filled(Q, fill_value)
-    np.fill_diagonal(Q, fill_value)
-    Q = 0.5 * (Q + Q.T)
-    return np.ma.getdata(Q)
-
+    cdef np.ndarray[np.double_t, ndim=2] sum_pdm = np.sum(pdm * np.invert(mask_2d), axis=1, keepdims=True)
+    cdef np.ndarray[np.double_t, ndim=2] sum_i = np.tile(sum_pdm, (1, leaf_node_count))
+    Q = (n_active - 2.) * pdm - sum_i - np.transpose(sum_i)
+    return Q, sum_pdm
 
 cdef unravel_index(np.int_t index, shape):
     out = []
     cdef np.int_t dim
     for dim in reversed(shape):
         out.append(index % dim)
-        index = np.floor(index / dim)
+        index = index / dim
     return tuple(reversed(out))
 
 cdef get_new_dist(
     np.ndarray[np.double_t, ndim=2] pdm,
     np.ndarray[np.uint8_t, ndim=1] mask,
     np.int_t f,
-    np.int_t g):
+    np.int_t g,
+    np.ndarray[np.double_t, ndim=2] sum_pdm):
     """Get neighbour joining distances to new node."""
-    cdef np.double_t eps = np.double(2.220446049250313e-16)
     # get distance all taxa to new node u
     cdef np.ndarray[np.double_t, ndim=1] dist_u = 0.5 * (pdm[f] + pdm[g] - pdm[f][g])
 
     # get distance from u to f
     cdef np.ndarray[np.uint8_t, ndim=1] mask_invert = np.invert(mask)
     cdef np.int_t n_active = np.maximum(sum(mask_invert), 3)
-    cdef np.ndarray[np.uint8_t, ndim=2] mask_invert_2d = np.outer(mask_invert, mask_invert)
-    cdef np.ndarray[np.double_t, ndim=1] sum_pdm = np.sum(pdm * mask_invert_2d, axis=-1)
-    cdef np.double_t dist_uf = 0.5 * pdm[f][g] + (sum_pdm[f] - sum_pdm[g]) / (2 * (n_active - 2))
+    cdef np.double_t n_active = np.maximum(np.sum(mask == 0), 3.)
+    cdef np.double_t dist_uf = 0.5 * pdm[f][g] + (sum_pdm[f] - sum_pdm[g]) / (2. * (n_active - 2.))
     dist_u[f] = np.maximum(dist_uf, eps)
     dist_u[g] = 0.0
     return dist_u
